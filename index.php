bba03604198414445d233cdbe245c13cdfe045fa
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-09 00:54:31 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Minh Hoang Nguyen</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+  <h1>Minh Hoang Nguyen</h1>
+  <h3>Swinburne</h3>
+</body>
