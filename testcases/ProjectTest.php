cb7df89d9f687d54112b6a0e393a71e30659d2c3
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-22 15:42:16 +0700

    Update ProjectTest.php

--- testcases/ProjectTest.php
+++ testcases/ProjectTest.php
@@ -10,7 +10,7 @@ class ProjectTest extends TestCase
 
         $project = new DevopsTest;
 
-        $project->project_name = "DevopsProject";
+        $project->project_name = "DevopsProject1";
 
         $this->assertEquals('DevopsProject', $project->getProjectName());
     }
