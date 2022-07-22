2b0616d67200b5414babfe02ac7bdbdde0bb7e4d
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-22 15:45:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>My Friend System - Home Page</title>
+    <title>Test Commit</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="framework/css/style.css" rel="stylesheet">
