14b623518c05572c768000cb8bc70b2513096f47
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-22 14:41:15 +0700

    Delete startover.php

--- startover.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start(); // Starts the session
-    $_SESSION = array(); // Unsets all session variables
-    session_destroy(); // Clears all data associated with the session
-    header("location:guessinggame.php"); // Redirects to guessinggame.php
-?>
