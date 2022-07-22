12cac9cf880ee2031091d2ad466ec05fe2a9c005
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-22 15:45:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Test Commit</title>
+    <title>Test Commit - 2</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="framework/css/style.css" rel="stylesheet">
