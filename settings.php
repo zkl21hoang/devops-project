3be2eb0072885efef2f39e3f89449c930c3db0c3
Hoang Minh Nguyen <75946931+zkl21hoang@users.noreply.github.com>
2022-07-29 14:31:29 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,5 +1,5 @@
 <?php
-    $host = "rds-devops-project.cf8wt323ong9.ap-southeast-1.rds.amazonaws.com";
+    $host = "devops-rds-database.cf8wt323ong9.ap-southeast-1.rds.amazonaws.com";
     $username = "sqladmin";
     $password = "Hoangsql123";
     $sql_database_name = "devops";
